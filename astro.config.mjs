80bb9a45b4048e237dae1e87bf73424f65dafd11
gustavo.silva <gustavo.silva@softplan.com.br>
2025-09-11 11:27:30 -0300

    feat: enhance deployment workflow and add Cloudflare adapter support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,18 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
   // integrations: [
   //   tailwind({
   //     ,
   //   }),
   // ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: cloudflare(),
 });
