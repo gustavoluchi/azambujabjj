bd69b5fff2b011c30e37430a4221555873a21931
gustavo.silva <gustavo.silva@softplan.com.br>
2025-09-11 11:38:22 -0300

    chore: remove Cloudflare adapter and update main entry in package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   // integrations: [
@@ -15,6 +13,4 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
-  adapter: cloudflare(),
 });
